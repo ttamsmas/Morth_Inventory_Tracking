b745dfcee1d08b9fc6413fc693b4c53bf57fa91e
ttamsmas <ttamsmas@gmail.com>
2020-11-06 15:29:22 -0800

    messing with local update items instead

--- curl-scripts/items/update.sh
+++ curl-scripts/items/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/items"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,9 +9,12 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "item": {
+      "key": "'"${KEY}"'",
+      "value": "'"${VALUE}"'"
     }
   }'
 
 echo
+
+# TOKEN=077064f173dbde25c19a5e0fc41a90e8 ID=5fa5ccb0e864eb0017250830 KEY="name" VALUE="hiC" sh curl-scripts-heroku/item/update.sh
