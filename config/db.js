3c5f45a31e196d6530ae25a471dcc5c7c56b6b92
ttamsmas <ttamsmas@gmail.com>
2020-11-05 16:00:00 -0800

    troubleshooting heroku naming syntax

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'morth_inventory_tracking'
+const mongooseBaseName = 'morthinventorytracking'
 
 // create the mongodb uri for development and test
 const database = {
