301ab716296b8cd87f6eaf849c50b156841e8b09
ttamsmas <ttamsmas@gmail.com>
2020-11-10 14:13:48 -0800

    changing the pluralization of the item database in the request

--- app/routes/item_routes.js
+++ app/routes/item_routes.js
@@ -52,10 +52,10 @@ router.get('/items/:id', requireToken, (req, res, next) => {
 })
 
 // POST / CREATE AN ITEM
-router.post('/item', requireToken, (req, res, next) => {
+router.post('/items', requireToken, (req, res, next) => {
   // set owner of new item to be the user signed in user
-  req.body.items.owner[0] = req.user.id
-  Item.create(req.body.items)
+  req.body.item.owner[0] = req.user.id
+  Item.create(req.body.item)
     // respond to succesful `create` with status 201 and JSON of new "item"
     .then(item => {
       res.status(201).json({ item: item })
